77a88eaae035e48b9b9eb8773e5ef121723c54a5
lyudakucher <kucherlyuda@gmail.com>
2016-03-18 23:50:32 +0200

    старт

---
+++ js/animation.js
@@ -0,0 +1,25 @@
+// http://codepen.io/Sp00ky/pen/wgHmj
+// $(document).ready(function() {
+
+//   var counter = function($this) {
+//     var maxNum = Math.abs(parseInt($this.text()));
+//     var i = 0;
+//     var repeat = maxNum / 50;
+
+//     setInterval(function() {
+
+//       $this.text(parseInt(i += repeat));
+
+//       if (i > maxNum) {
+//         $this.text(parseInt(maxNum));
+//         return;
+//       }
+
+//     }, 40);
+//   }
+
+//   $('.num').each(function(index, element) {
+//     counter($(element));
+//   });
+
+// });
